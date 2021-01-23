8f1ea3a670fd69ebde252b001d5b36086e3f1f3d
kmolski <starfighter413@gmail.com>
2021-01-23 16:30:00 +0100

    Add simple echo app for testing

---
+++ app/src/main/java/Client.java
@@ -0,0 +1,18 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        var client = new MqttClient(Secrets.MQTT_SERVER_URL, "client");
+        var options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setConnectionTimeout(5);
+        client.connect(options);
+
+        System.out.println("Connected to host " + client.getServerURI() +"!");
+
+        client.subscribe(Secrets.MQTT_TOPIC, (topic, msg) -> {
+            System.out.println("New message on topic `" + topic + "`: " + msg);
+        });
+    }
+}
