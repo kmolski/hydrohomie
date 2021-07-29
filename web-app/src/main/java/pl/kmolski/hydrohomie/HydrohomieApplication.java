cd0e8c4e2044a439ae36c65369a16903eb4054ed
kmolski <starfighter413@gmail.com>
2021-07-29 07:15:45 +0200

    Add Spring Boot web application

---
+++ web-app/src/main/java/pl/kmolski/hydrohomie/HydrohomieApplication.java
@@ -0,0 +1,13 @@
+package pl.kmolski.hydrohomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrohomieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydrohomieApplication.class, args);
+	}
+
+}
---
+++ web-app/src/test/java/pl/kmolski/hydrohomie/HydrohomieApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kmolski.hydrohomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrohomieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
