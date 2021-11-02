4ddbe57da8a01fac01e91e048c447af1f0d5ac6c
kmolski <starfighter413@gmail.com>
2021-11-02 16:56:28 +0100

    Add custom login page template

--- web-app/src/main/java/pl/kmolski/hydrohomie/config/SecurityConfiguration.java
+++ web-app/src/main/java/pl/kmolski/hydrohomie/config/SecurityConfiguration.java
@@ -3,8 +3,14 @@ package pl.kmolski.hydrohomie.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -13,5 +19,26 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        var logoutSuccessHandler = new RedirectServerLogoutSuccessHandler();
+        logoutSuccessHandler.setLogoutSuccessUrl(URI.create("/login?logout"));
+
+        return http.authorizeExchange()
+                .pathMatchers("/login", "/webjars/**").permitAll()
+                .pathMatchers("/**").authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/"))
+                    .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessHandler(logoutSuccessHandler)
+                    .and()
+                .csrf().disable()
+                .build();
     }
 }
