5811d647ce85b2c0c6b86fe9c21b3a294f6a0206
kmolski <starfighter413@gmail.com>
2021-11-02 16:58:21 +0100

    Change configuration prefix for admin account and MQTT client

--- web-app/src/main/java/pl/kmolski/hydrohomie/config/MqttClientSettings.java
+++ web-app/src/main/java/pl/kmolski/hydrohomie/config/MqttClientSettings.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "mqtt")
+@ConfigurationProperties("mqtt")
 public class MqttClientSettings {
 
     private String url;
